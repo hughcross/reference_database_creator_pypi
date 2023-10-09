0665b133d0f248c2a4d1a93f7eed0ed23326b4b2
Hugh Cross <hughbcross@gmail.com>
2023-10-09 12:01:20 -0600

    added pypi packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='crabs',
       description='CRABS: Creating Reference databases for Amplicon-Based Sequencing',
       author='Gert-Jan Jeunen',
 	  author_email='gjeunen@gmail.com',
 	  url='https://github.com/gjeunen/reference_database_creator',
-	  version='0.1.8',
-      packages=['function'],
-      scripts=['crabs']
+	  version='0.2.0',
+      packages=['crabs'],
+      install_requires=[
+          'biopython >= 1.78',
+          'argparse',
+          'tqdm',
+          'numpy',
+          'pandas >=0.23.4',
+          'matplotlib'
+      ],
+      python_requires=">=3.6",
+      entry_points={
+          "console_scripts": [
+              "crabs = crabs.crabs:main"
+          ]
+      }
 )
